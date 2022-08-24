b6806d4546a6128b310278eb17eeeff0744690f1
syswing <1102977704@qq.com>
2022-08-24 16:46:34 +0800

    update

---
+++ components/levelSystem.lua
@@ -0,0 +1,28 @@
+-- 升级组件
+local MAX_LEVEL = 600
+local START_LEVEL_EXP = 100
+-- local 
+local LevelSystem = Class(function(self,inst)
+	self.inst = inst 
+	self.current_exp = 0
+	self.current_level = 0
+end)
+
+function LevelSystem:UpLevel()
+	if(self.current_level < 600)
+		return self.current_level++
+	end
+end
+
+function LevelSystem:GetLevel()
+	return self.current_level
+end
+
+function LevelSystem:GetExp()
+	return self.current_exp
+end
+
+function LevelSystem:AddExp(exp){
+	return self.current_exp += exp
+}
+
---
+++ modinfo.lua
@@ -0,0 +1,33 @@
+name = "react-dst"
+description = "demo"
+forumthread = ""
+author = "sword"
+
+version =            "1.01"
+version_compatible = "1.00"
+
+api_version = 10
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+dst_compatible = true
+all_clients_require_mod = false
+client_only_mod = false
+
+
+icon_atlas = "preview.xml"
+icon = "preview.tex"
+
+configuration_options =
+{
+    {
+        name = "show_oneself",
+        label = "Show Oneself",
+		hover = "Show your nickname of your character.",
+        options = 
+        {
+            {description = "Yes", data = true},
+            {description = "No", data = false},
+        }, 
+        default = true,
+    },
+}
